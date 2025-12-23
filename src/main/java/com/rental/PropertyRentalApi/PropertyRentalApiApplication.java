7bb9b551176eef763f268481a4f7e166e688c536
neathcmd <ksdf71464@gmail.com>
2025-12-23 07:46:20 +0700

    first commit

---
+++ src/main/java/com/rental/PropertyRentalApi/PropertyRentalApiApplication.java
@@ -0,0 +1,13 @@
+package com.rental.PropertyRentalApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyRentalApiApplication.class, args);
+        System.out.println("Hello java");
+	}
+}
---
+++ src/test/java/com/rental/PropertyRentalApi/PropertyRentalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.PropertyRentalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRentalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
