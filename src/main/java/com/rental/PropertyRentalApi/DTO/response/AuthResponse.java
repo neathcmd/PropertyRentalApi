ed794c313a2a337ebf1b10fc3b43072fa8e42444
neathcmd <ksdf71464@gmail.com>
2025-12-26 10:58:59 +0700

    create DTO Request and Response

---
+++ src/main/java/com/rental/PropertyRentalApi/DTO/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.rental.PropertyRentalApi.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String message;
+    private UserResponse user;
+}
--- src/main/java/com/rental/PropertyRentalApi/Service/Impl/AuthServiceImpl.java
+++ src/main/java/com/rental/PropertyRentalApi/Service/Impl/AuthServiceImpl.java
@@ -1,9 +1,12 @@
-package com.rental.PropertyRentalApi.Service;
+package com.rental.PropertyRentalApi.Service.Impl;
 
 import com.rental.PropertyRentalApi.DTO.request.RegisterRequest;
 import com.rental.PropertyRentalApi.DTO.response.RegisterResponse;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-public interface AuthService {
-    RegisterResponse register(RegisterRequest request);
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl {
 }
