73cc49bb4bca20eeb50b590576cc5165e4490d38
neathcmd <ksdf71464@gmail.com>
2025-12-31 14:57:10 +0700

    ready to merg to develop

--- src/main/java/com/rental/PropertyRentalApi/Scripts/RoleSeeder.java
+++ src/main/java/com/rental/PropertyRentalApi/Scripts/RoleSeeder.java
@@ -1,13 +1,13 @@
-package com.rental.PropertyRentalApi.Scripts;
-
-import com.rental.PropertyRentalApi.Repository.RoleRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class RoleSeeder implements CommandLineRunner {
-
-    private final RoleRepository roleRepository;
-}
+//package com.rental.PropertyRentalApi.Scripts;
+//
+//import com.rental.PropertyRentalApi.Repository.RoleRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class RoleSeeder implements CommandLineRunner {
+//
+//    private final RoleRepository roleRepository;
+//}
