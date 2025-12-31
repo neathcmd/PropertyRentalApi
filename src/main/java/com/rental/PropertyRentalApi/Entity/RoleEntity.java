cc6589c3123f5b92ca90795d69a64ad7a2556999
neathcmd <ksdf71464@gmail.com>
2025-12-31 14:55:42 +0700

    working on data seeding

---
+++ src/main/java/com/rental/PropertyRentalApi/Entity/RoleEntity.java
@@ -0,0 +1,29 @@
+package com.rental.PropertyRentalApi.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserEntity> users = new HashSet<>();
+}
--- src/main/java/com/rental/PropertyRentalApi/Entity/UserEntity.java
+++ src/main/java/com/rental/PropertyRentalApi/Entity/UserEntity.java
@@ -1,16 +1,26 @@
 package com.rental.PropertyRentalApi.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity implements UserDetails {
 
     @Id
@@ -32,8 +42,18 @@ public class UserEntity implements UserDetails {
     @Column(name = "phone",unique = true)
     private String phone;
 
-    // @Column(nullable = false)
-    // private String role; 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roles = new HashSet<>();
+
+
+//    public static ElasticsearchConnectionDetails.Node builder() {
+//        return null;
+//    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
